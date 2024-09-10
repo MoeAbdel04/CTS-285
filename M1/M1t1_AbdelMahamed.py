1ad4b5622ab6d723e6386e49c77cad1c223e3207
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-09-10 13:25:58 -0400

    Add files via upload
    Intro assignment

---
+++ M1/M1t1_AbdelMahamed.py
@@ -0,0 +1,17 @@
+# CTS 285
+# M1t1
+# Mahamed Abdel
+# 8/19/24
+
+
+
+
+print("Hello my name is Mahamed Abdel.") 
+print("Im studying java and python this semester.")
+print("I have interests in cars, crypto, stocks, working out and technology.")
+
+
+
+
+
+
