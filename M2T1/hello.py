2472f3a0341559bec4c14d906fb1c5999979379b
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-09-11 23:31:50 +0000

    mistake

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
+
+@app.route("/submit-text", methods=["POST"])
+def submit_text():
+    # Get the text from the input field
+    user_input = request.form["user_input"]
+    return f"You entered: {user_input}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
