f68b8398f5241901e6954bedcfc28c89f4ec5469
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-09-12 10:07:16 +0000

    added templates folder and main page

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request
+# minimal Flask app
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # three quotes for multiline strings
+    name = "Sarah"
+    return render_template("main_page.html", name=name)
+    
 
-@app.route("/submit-text", methods=["POST"])
-def submit_text():
-    # Get the text from the input field
-    user_input = request.form["user_input"]
-    return f"You entered: {user_input}"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    
+@app.route("/action")
+def action():
+    return "Hello from the action route!"
