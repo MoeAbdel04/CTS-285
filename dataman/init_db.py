05d9b1e150a419518db73ff085b6edd79b830f18
MoeAbdel04 <145929908+MoeAbdel04@users.noreply.github.com>
2024-10-05 11:29:32 +0000

    updated init.db

--- dataman/init_db.py
+++ dataman/init_db.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+
 from dataman.app import db
 db.create_all()
 print("Database tables created successfully.")
